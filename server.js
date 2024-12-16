4786c323cba3d08465e25e40c0e16ba5dc970c39
Haikal Risnandar <haikalrisnandar82@gmail.com>
2024-12-16 16:29:22 +0700

    test change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dboperations = require('./query');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.get('/barang', (req, res) => {
     dboperations.getbarang((error, result) => {
