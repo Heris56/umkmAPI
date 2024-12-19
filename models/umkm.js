d868f92964ccaa3bd34b94a9c2716cc1b9b03ea3
Haikal Risnandar <haikalrisnandar82@gmail.com>
2024-12-19 21:41:38 +0700

    nambah model umkm, fix model produk, nambah query, nambah server route

---
+++ models/umkm.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const UMKM = sequelize.define('UMKM', {
+    id: {
+        field: 'id_umkm', // Nama id di database
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nama_lengkap: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nomor_telepon: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    alamat: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nama_usaha: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    NIK_KTP: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+
+}, {
+    tableName: 'umkm',
+    timestamps: false
+});
+
+module.exports = UMKM;
