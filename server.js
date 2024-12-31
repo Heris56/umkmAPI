7847772b588360f4c89dcfd1b8cd5d9743ad4ffc
zfersya <zfersya@gmail.com>
2024-12-31 15:52:34 +0700

    add inbox route

--- server.js
+++ server.js
@@ -576,6 +576,17 @@ app.put('/updatepassword/:email/:newPassword', (req, res) => {
 
 // End Server Dapa
 
+app.get('/getinboxpesanan', (req, res) => {
+    dboperations.getinboxpesanan((error, result) => {
+        if (error) {
+            console.error('error get inbox:', error);
+            return res.status(500).send('error fetch inbox pesanan');
+        }
+        res.json(result);
+    });
+});
+// server.js
+
 app.listen(port, () => {
     console.log(`server berjalan di ${port}`);
 });
