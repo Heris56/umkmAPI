c5c8a8f42c1d5629632f3b2b417a7577dc8b8da7
Haikal Risnandar <115600487+Heris56@users.noreply.github.com>
2025-04-26 01:43:31 +0700

    Update R2Services.js
    test http

--- R2Services.js
+++ R2Services.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const r2 = new S3Client({
     region: "auto",
-    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    endpoint: `http://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
     credentials: {
         accessKeyId: process.env.R2_ACCESS_KEY_ID,
         secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
